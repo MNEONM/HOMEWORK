566919a2b1d3d5f5e20388905eca6fbe04741c18
MNEONM <43368438+MNEONM@users.noreply.github.com>
2018-11-06 10:38:10 +0200

    Add files via upload

---
+++ POSNEG.java
@@ -0,0 +1,16 @@
+package pos_neg;
+import java.io.*;
+import java.util.Scanner;
+
+public class POSNEG {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int[] array=new int[5];
+        int neg=0;
+        int pos=0;
+        int[] array_pos=new int[5];
+        for (int i = 0; i < array.length; i++) {
+            array[i]=input.nextInt();
+        }
+    }
+}
